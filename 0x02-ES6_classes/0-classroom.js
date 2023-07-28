bae6ef6fa9ec906ce01de62f4226e097f0684942
Eminent6916 <ologelisha@gmail.com>
2023-07-28 04:08:07 +0000

    class1

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+*Represents a class room
+*/
+export default class ClassRoom{
+    /**
+    * Create a new @see {@link ClassRoom}
+    * @param {Number} maxStudentsSize - The maximum number of students in the class
+    */
+    construct(maxStudentsSize){
+        this._maxStudentsSize
+    }
+}
+
